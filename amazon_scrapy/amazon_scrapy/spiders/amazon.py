b5c561a6d436ae3c626b52deaf6d47658a955826
MOHIT-0000 <mohit000.py@gmail.com>
2021-12-07 17:02:44 +0530

    updated

--- amazon_scrapy/amazon_scrapy/spiders/amazon.py
+++ amazon_scrapy/amazon_scrapy/spiders/amazon.py
@@ -12,25 +12,26 @@ class AmazonSpider(scrapy.Spider):
     allowed_domains = ['amazon.in']
     page=2
     next_urls_int=0
-    key_word=input("Product name -  ")
-    start_urls = ["https://www.amazon.in/s?k="+(key_word)]
+    key_word=input("Product name->")
+    start_urls = [
+        "https://www.amazon.in/s?k="+(key_word)+"&ref=nb_sb_noss_2"
+        ]
     numbers=0
     def parse(self,response):
         for sel in response.css(".s-widget-spacing-small"):
             item = AmazonScrapyItem()
-            try:
-                item['title'] = sel.xpath(".//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-2']//span//text()").get()
-            except:
-                item['title'] = sel.xpath('.//*[@class="a-size-medium a-color-base a-text-normal"]//text()').get()
+            title = sel.xpath(".//h2[@class='a-size-mini a-spacing-none a-color-base s-line-clamp-2']//span//text()").get()
+            if title is None:
+                title=sel.xpath('.//*[@class="a-size-medium a-color-base a-text-normal"]//text()').extract_first()
+            item['title']=title
             item['price'] = sel.css('.a-price-whole::text').extract_first()
             item['rating'] = sel.xpath('.//*[contains(concat( " ", @class, " " ), concat( " ", "aok-align-bottom", " " ))]//text()').extract_first()
             item['total_review'] = sel.xpath('.//*[@class="a-row a-size-small"]//a/span/text()').extract_first()
             item['image_url'] = sel.css('img.s-image::attr(src)').extract_first()
             yield item
         next_test = response.xpath(".//*[@class='a-text-center']//li[@class='a-disabled']/text()").extract()
-        if AmazonSpider.numbers<1:
+        if AmazonSpider.page<=2:
             AmazonSpider.next_urls_int=next_test[-1]
-            AmazonSpider.numbers+=1
         else:
             pass
         if AmazonSpider.page<=int(AmazonSpider.next_urls_int):
